eef99031cd6b8254f04a4e5d3fd241fde8f1f2b8
Jerry Clinesmith <jerryclinesmith@gmail.com>
2012-11-14 14:32:48 -0600

    Minor refactoring

--- lib/mongoid-encrypted-fields/fields/encrypted_field.rb
+++ lib/mongoid-encrypted-fields/fields/encrypted_field.rb
@@ -5,11 +5,11 @@ module Mongoid
     extend ActiveSupport::Concern
 
     def encrypted
-      if (self.try(:frozen?))
-        @encrypted ||= self.class.encrypt(self.to_s)
+      if frozen?
+        @encrypted ||= self.class.encrypt(to_s)
       else
         # We are mutable - need to encrypt whenever asked
-        self.class.encrypt(self.to_s)
+        self.class.encrypt(to_s)
       end
     end
 
--- lib/mongoid-encrypted-fields/version.rb
+++ lib/mongoid-encrypted-fields/version.rb
@@ -1,5 +1,5 @@
 module Mongoid
   module EncryptedFields
-      VERSION = "0.0.5"
+      VERSION = "0.0.6"
   end
 end
