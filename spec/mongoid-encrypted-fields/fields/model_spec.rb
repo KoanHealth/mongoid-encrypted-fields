7c37cb1f3b7750f22557d9c71007573f14aee6f4
Jerry Clinesmith <jerryclinesmith@gmail.com>
2013-03-19 20:32:23 -0500

    Add an address Hash to the test Person model

--- spec/mongoid-encrypted-fields/fields/model_spec.rb
+++ spec/mongoid-encrypted-fields/fields/model_spec.rb
@@ -10,7 +10,9 @@ describe 'Single model' do
   let (:ssn_encrypted) { Mongoid::EncryptedString.mongoize(ssn) }
   let (:birth_date) { 20.years.ago.to_date }
   let (:birth_date_encrypted) { Mongoid::EncryptedDate.mongoize(birth_date) }
-  let (:person) { Person.new(name: 'John Doe', ssn: ssn, birth_date: birth_date) }
+  let (:address) { {street: '123 Main St', city: 'Anytown', state: 'TX' } }
+  let (:address_encrypted) { Mongoid::EncryptedHash.mongoize(address) }
+  let (:person) { Person.new(name: 'John Doe', ssn: ssn, birth_date: birth_date, address: address) }
 
   it "model stores encrypted ssn" do
     person.attributes['ssn'].should eq(ssn_encrypted)
@@ -20,12 +22,20 @@ describe 'Single model' do
     person.attributes['birth_date'].should eq(birth_date_encrypted)
   end
 
+  it "model stores encrypted address" do
+    person.attributes['address'].should eq(address_encrypted)
+  end
+
   it "ssn getter returns raw value" do
     person.ssn.should eq(ssn)
   end
 
   it "birth_date getter returns raw value" do
     person.birth_date.should eq(birth_date)
+  end
+
+  it "address getter returns raw value" do
+    person.address.should eq(address)
   end
 
   describe "after save" do
@@ -44,12 +54,20 @@ describe 'Single model' do
       @persisted.attributes['birth_date'].should eq(birth_date_encrypted)
     end
 
+    it "encrypted address is persisted" do
+      @persisted.attributes['address'].should eq(address_encrypted)
+    end
+
     it "encrypted ssn getter returns raw value" do
       @persisted.ssn.should eq(ssn)
     end
 
     it "encrypted birth_date getter returns raw value" do
       @persisted.birth_date.should eq(birth_date)
+    end
+
+    it "encrypted address getter returns raw value" do
+      @persisted.address.should eq(address)
     end
 
   end
--- spec/support/models/person.rb
+++ spec/support/models/person.rb
@@ -4,5 +4,6 @@ class Person
   field :name, type: String
   field :ssn, type: Mongoid::EncryptedString
   field :birth_date, type: Mongoid::EncryptedDate
+  field :address, type: Mongoid::EncryptedHash
 
 end
