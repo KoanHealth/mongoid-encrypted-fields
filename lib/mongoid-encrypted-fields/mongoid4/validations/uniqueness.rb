9670f4b34480b9ae3ee2ab947f8cc5a5fe08d502
Jerry Clinesmith <jerryclinesmith@gmail.com>
2013-12-09 21:30:03 -0600

    Mongoid 4 validations

---
+++ lib/mongoid-encrypted-fields/mongoid4/validations/uniqueness.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+module Mongoid
+  module Validatable
+
+    # Monkey-patch for Mongoid's uniqueness validator to enforce that the :case_sensitive option does not work
+    # for encrypted fields; they must always be case-sensitive.
+    # Patch is confirmed to work on Mongoid >= 4.0.0
+    class UniquenessValidator
+
+      def setup_with_validation(klass)
+        setup_without_validation(klass)
+        return if case_sensitive?
+        attributes.each do |attribute|
+          field_type = @klass.fields[@klass.database_field_name(attribute)].options[:type]
+          raise ArgumentError, "Encrypted field :#{attribute} cannot support case insensitive uniqueness" if field_type.method_defined?(:encrypted)
+        end
+      end
+
+      alias_method :setup_without_validation, :setup
+      alias_method :setup, :setup_with_validation
+
+    end
+  end
+end
