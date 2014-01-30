7f1f88966b1c535f3befef9966f495623470f629
Jerry Clinesmith <jerryclinesmith@gmail.com>
2014-01-30 11:35:28 -0600

    Issue #14:  Attempting to adapt to changes in Mongoid 4.0 and ActiveModel 4.1

--- lib/mongoid-encrypted-fields/mongoid4/validations/uniqueness.rb
+++ lib/mongoid-encrypted-fields/mongoid4/validations/uniqueness.rb
@@ -8,12 +8,32 @@ module Mongoid
     # Patch is confirmed to work on Mongoid >= 4.0.0
     class UniquenessValidator
 
-      def check_validity!
-        return if case_sensitive?
-        attributes.each do |attribute|
-          field_type = options[:class].fields[options[:class].database_field_name(attribute)].options[:type]
-          raise ArgumentError, "Encrypted field :#{attribute} cannot support case insensitive uniqueness" if field_type.method_defined?(:encrypted)
+      # Setup has been deprecated in ActiveModel 4.1
+      if instance_methods(false).include?(:setup)
+
+        def setup_with_validation(klass)
+          setup_without_validation(klass)
+          return if case_sensitive?
+          attributes.each do |attribute|
+            field_type = @klass.fields[@klass.database_field_name(attribute)].options[:type]
+            raise ArgumentError, "Encrypted field :#{attribute} cannot support case insensitive uniqueness" if field_type.method_defined?(:encrypted)
+          end
         end
+
+        alias_method :setup_without_validation, :setup
+        alias_method :setup, :setup_with_validation
+
+      else
+
+        # ActiveModel 4.1+ adds options[:class] to reference the document that included this validation
+        def check_validity!
+          return if case_sensitive?
+          attributes.each do |attribute|
+            field_type = options[:class].fields[options[:class].database_field_name(attribute)].options[:type]
+            raise ArgumentError, "Encrypted field :#{attribute} cannot support case insensitive uniqueness" if field_type.method_defined?(:encrypted)
+          end
+        end
+
       end
 
     end
