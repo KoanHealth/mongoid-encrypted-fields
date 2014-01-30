68d89fc76590347d32d6c312362ba40e6586a110
Jerry Clinesmith <jerryclinesmith@gmail.com>
2014-01-30 08:59:00 -0600

    Move our validation check into the aptly named check_validity!

--- lib/mongoid-encrypted-fields/mongoid4/validations/uniqueness.rb
+++ lib/mongoid-encrypted-fields/mongoid4/validations/uniqueness.rb
@@ -8,17 +8,13 @@ module Mongoid
     # Patch is confirmed to work on Mongoid >= 4.0.0
     class UniquenessValidator
 
-      def setup_with_validation(klass)
-        setup_without_validation(klass)
+      def check_validity!
         return if case_sensitive?
         attributes.each do |attribute|
           field_type = @klass.fields[@klass.database_field_name(attribute)].options[:type]
           raise ArgumentError, "Encrypted field :#{attribute} cannot support case insensitive uniqueness" if field_type.method_defined?(:encrypted)
         end
       end
-
-      alias_method :setup_without_validation, :setup
-      alias_method :setup, :setup_with_validation
 
     end
   end
