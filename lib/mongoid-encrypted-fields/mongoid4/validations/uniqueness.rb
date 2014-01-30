66f75ab3007a3882219edd6c4ceb9afeac21320f
Jerry Clinesmith <jerryclinesmith@gmail.com>
2014-01-30 15:22:27 -0600

    Issue #14: Refactor solution - method to abstract how we get a reference back to the validating document

--- lib/mongoid-encrypted-fields/mongoid4/validations/uniqueness.rb
+++ lib/mongoid-encrypted-fields/mongoid4/validations/uniqueness.rb
@@ -8,35 +8,20 @@ module Mongoid
     # Patch is confirmed to work on Mongoid >= 4.0.0
     class UniquenessValidator
 
-      # Setup has been deprecated in ActiveModel 4.1
-      if instance_methods(false).include?(:setup)
-
-        def setup_with_validation(klass)
-          setup_without_validation(klass)
-          return if case_sensitive?
-          attributes.each do |attribute|
-            field_type = @klass.fields[@klass.database_field_name(attribute)].options[:type]
-            raise ArgumentError, "Encrypted field :#{attribute} cannot support case insensitive uniqueness" if field_type.method_defined?(:encrypted)
-          end
+      def check_validity!
+        return if case_sensitive?
+        return unless (doc = owner_document)
+        attributes.each do |attribute|
+          field_type = doc.fields[doc.database_field_name(attribute)].options[:type]
+          raise ArgumentError, "Encrypted field :#{attribute} cannot support case insensitive uniqueness" if field_type.method_defined?(:encrypted)
         end
+      end
 
-        alias_method :setup_without_validation, :setup
-        alias_method :setup, :setup_with_validation
-
-      else
-
-        # ActiveModel 4.1+ adds options[:class] to reference the document that included this validation
-        def check_validity!
-          return if case_sensitive?
-          valid_document = options[:class]
-          # If we're on a version of ActiveModel that doesn't have setup or pass options[:class], we have no reference to the document
-          return unless valid_document
-          attributes.each do |attribute|
-            field_type = valid_document.fields[valid_document.database_field_name(attribute)].options[:type]
-            raise ArgumentError, "Encrypted field :#{attribute} cannot support case insensitive uniqueness" if field_type.method_defined?(:encrypted)
-          end
-        end
-
+      # Older versions of Mongoid's UniquenessValidator have a klass variable to reference the validating document
+      # This was later replaced in ActiveModel with options[:class]
+      def owner_document
+        return klass if instance_methods(false).include?(:klass)
+        options[:class]
       end
 
     end
