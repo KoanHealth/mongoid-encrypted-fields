ccef1015d8a9db121ea4738c3cfbaa2811038690
Jerry Clinesmith <jerryclinesmith@gmail.com>
2015-06-11 08:39:13 -0500

    Update config file for Mongoid 5

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,11 +16,17 @@ require 'rspec'
 
 require 'mongoid-encrypted-fields'
 
-Dir["#{File.dirname(__FILE__)}/../examples/**/*.rb"].each {|f| require f}
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir["#{File.dirname(__FILE__)}/../examples/**/*.rb"].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 ENV['MONGOID_ENV'] ||= 'test'
-Mongoid.load!("#{File.dirname(__FILE__)}/config/mongoid.yml")
+
+# Config format changed in mongoid 5
+if Mongoid::EncryptedFields.mongoid_major_version < 5
+  Mongoid.load!("#{File.dirname(__FILE__)}/config/mongoid.yml")
+else
+  Mongoid.load!("#{File.dirname(__FILE__)}/config/mongoid5.yml")
+end
 
 Mongoid::EncryptedFields.logger.level = Logger::FATAL
 
